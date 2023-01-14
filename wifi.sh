1c705ee73f233a497c675f1160a0627e8af90034
rustammahilane <rustammahilane@gmail.com>
2023-01-14 23:14:37 +0530

    updated wifi.sh

--- wifi.sh
+++ wifi.sh
@@ -1,10 +1,34 @@
 #! /bin/bash
 
+<<	 WIFI_TOGGLE
+	This shell scripts toggles the wifi switch ON/OFF.
+
+	Connections:
+	to show network interface cards 
+	$ ifconfig
+
+	to turn ON or OFF
+	$ nmcli radio wifi on/off
+
+	to check status of network interface cards
+	$ nmcli device status
+	
+	to scan available networks
+	$ nmcli device wifi list
+	or
+	$ sudo iw wlan0 scan | grep SSID
+
+	connecting
+	$ nmcli --ask device wifi connect <SSID>
+WIFI_TOGGLE
+
 wi="nmcli radio wifi"
 
-if wifi | grep "off" -wn;
+if wifi | grep "off" -w;
 then
 $wi on
 else
 $wi off
 fi
+
+wifi
